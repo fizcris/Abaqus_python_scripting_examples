9f1a410232a058dd6aee47beeaa275d1c70b7b76
fizcris <fizcris@gmail.com>
2021-01-04 12:19:12 +0100

    Minor dimensional fix

--- probeta_2_full.py
+++ probeta_2_full.py
@@ -1,4 +1,4 @@
- # coding=utf-8
+# coding=utf-8
 from part import *
 from material import *
 from section import *
@@ -12,7 +12,7 @@ from sketch import *
 from regionToolset import *
 from visualization import *
 from connectorBehavior import *
-import  regionToolset
+import regionToolset
 import time
 
 ##################      Helper garbage colect. funct.         ##################
@@ -61,7 +61,7 @@ for i in range(3):
     CircleByDiameter(31.0 + 40.0 * 4.0, y, 5.0)
 
 # Create force hole
-CircleByDiameter(190.5, 40.0, 8.0)
+CircleByDiameter(191, 40.0, 8.0)
 
 # Create set 2 of holes
 for i in range(4):
@@ -105,7 +105,7 @@ mdb.models['Model-1'].ConstrainedSketch(gridSpacing=10.77, name='__profile__',
                                                                             sketchOrientation=RIGHT,
                                                                             origin=(0.0, 0.0, 0.0)))
 part1.projectReferencesOntoSketch(filter=COPLANAR_EDGES, sketch=mdb.models['Model-1'].sketches['__profile__'])
-CircleByDiameter(190.5, 40.0, 15.0)
+CircleByDiameter(191, 40.0, 15.0)
 part1.PartitionFaceBySketch(faces=part1.faces, sketch=mdb.models['Model-1'].sketches['__profile__'])
 del mdb.models['Model-1'].sketches['__profile__']
 ######### Partition Load Cell Sensor
@@ -161,11 +161,10 @@ part1.setMeshControls(elemShape=QUAD, regions=part1.faces, algorithm=mesh.MEDIAL
                       allowMapped=True)
 #########  Set element type
 elemType1 = mesh.ElemType(elemCode=mesh.S4R, elemLibrary=STANDARD,
-    secondOrderAccuracy=OFF, hourglassControl=DEFAULT)
+                          secondOrderAccuracy=OFF, hourglassControl=DEFAULT)
 elemType2 = mesh.ElemType(elemCode=mesh.S3, elemLibrary=STANDARD)
 
-
-pickedRegions =(part1.faces, )
+pickedRegions = (part1.faces,)
 part1.setElementType(regions=pickedRegions, elemTypes=(elemType1, elemType2))
 part1.generateMesh()
 
@@ -173,24 +172,24 @@ part1.generateMesh()
 a = mdb.models['Model-1'].rootAssembly
 f1 = a.instances['Part-1-1'].faces
 faces1 = f1.getSequenceFromMask(mask=(
-    '[#800048bf #120001 #2400006 #600 #c05000 #4 ]', ), )
+    '[#800048bf #120001 #2400006 #600 #c05000 #4 ]',), )
 e1 = a.instances['Part-1-1'].edges
 edges1 = e1.getSequenceFromMask(mask=(
     '[#1c771dce #81f0001c #3 #0 #7e #7800000 #3',
-    ' #3f00 #0 #3078000 #0 #3f8000 #0:2 #3ee0000', ' #e0000 #3 ]', ), )
+    ' #3f00 #0 #3078000 #0 #3f8000 #0:2 #3ee0000', ' #e0000 #3 ]',), )
 v1 = a.instances['Part-1-1'].vertices
 verts1 = v1.getSequenceFromMask(mask=(
-    '[#43162cc #21f00 #0 #f #f0000007 #0 #e00', ' #3e0000 #0 #3f00 ]', ), )
+    '[#43162cc #21f00 #0 #f #f0000007 #0 #e00', ' #3e0000 #0 #3f00 ]',), )
 region = regionToolset.Region(vertices=verts1, edges=edges1, faces=faces1)
 mdb.models['Model-1'].EncastreBC(name='BC-2', createStepName='Initial',
-    region=region, localCsys=None)
+                                 region=region, localCsys=None)
 
 ##################      Create Load  Step         ##################
 mdb.models['Model-1'].StaticStep(description='Aplicacion presion', name='Step-1',
                                  previous='Initial')
 
 ##################      Create Load          ##################
-  # N
+# N
 ##################      Helper garbage colect. funct.         ##################
 if 'fuerza' not in locals():
     fuerza = 14.86
@@ -206,15 +205,15 @@ mdb.models['Model-1'].Pressure(amplitude=UNSET, createStepName='Step-1',
 
 ##################      Data extraction        ##################
 #########  Create Set for Load cell
-part1.Set(faces=part1.faces.getSequenceFromMask(mask=('[#0:4 #20000000 #10 ]', ),),
+part1.Set(faces=part1.faces.getSequenceFromMask(mask=('[#0:4 #20000000 #10 ]',), ),
           name='Galga')
-
 
 #########  Create Field Output Request
 mdb.models['Model-1'].FieldOutputRequest(createStepName='Step-1',
-                                        name='F-Output-2', rebar=EXCLUDE,
-                                        region=mdb.models['Model-1'].rootAssembly.allInstances['Part-1-1'].sets['Galga'],
-                                        sectionPoints=DEFAULT, variables=('S', 'E'))
+                                         name='F-Output-2', rebar=EXCLUDE,
+                                         region=mdb.models['Model-1'].rootAssembly.allInstances['Part-1-1'].sets[
+                                             'Galga'],
+                                         sectionPoints=DEFAULT, variables=('S', 'E'))
 ##################      Create Job/s          ##################
 # Check student
 numNodes = len(part1.nodes);
